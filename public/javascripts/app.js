3de3a1b4f015e37c5a7a0b51a5cde0ae38a2f2e4
Sebastian Bueno <juan.bueno01@usc.edu.co>
2015-08-26 02:05:56 -0500

    bug clicks

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,13 +1,30 @@
-var app = angular.module('tabla_juegos',['ngRoute']).controller('ctrl_principal',function($http,$scope){
+var app = angular.module('tabla_juegos',['ngRoute'])
+
+app.controller('ctrl_principal',function($http,$scope, $window , $location){
+    
 		$http.get('jsonTest.json').then(function(response){
 				$scope.data=response.data;
 		});
+    
+    $scope.eliminacion = function ($window , $location) {
+        $window.alert("Vas a ver un ejemplo Sencillo");
+//        $location.path("/singleElimination");
+    };
+    
+    $scope.alert = function ( $window ) {
+        $window.alert("hola");
+        
+    };
 });
 
 app.config(function ($routeProvider) {
     $routeProvider.when("/" , {
         controller : "ctrl_principal",
         templateUrl: "../templates/tabla.html"
+    })
+    .when("/singleElimination" , {
+        controller:"ctrl_principal",
+        templateUrl:"../templates/single.html"
     })
     .otherwise({
         reditecTo : "/"
