2665a88855beb4d75b7b56b3efc1059872d6302f
Sebastian Bueno <juan.bueno01@usc.edu.co>
2015-08-26 01:36:47 -0500

    Tabla embebida con angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,17 @@
-angular.module('tabla_juegos',[]).controller('ctrl_principal',function($http,$scope){
+var app = angular.module('tabla_juegos',['ngRoute']).controller('ctrl_principal',function($http,$scope){
 		$http.get('jsonTest.json').then(function(response){
 				$scope.data=response.data;
 		});
 });
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/" , {
+        controller : "ctrl_principal",
+        templateUrl: "../templates/tabla.html"
+    })
+    .otherwise({
+        reditecTo : "/"
+    })
+    
+});
+
