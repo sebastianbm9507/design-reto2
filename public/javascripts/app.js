4c7ef9ef734d9e07afba39281dd4ab8636ffdc4f
Sebastian Bueno <juan.bueno01@usc.edu.co>
2015-08-26 22:17:27 -0500

    Arreglo bugs de eventp

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,21 +1,24 @@
 var app = angular.module('tabla_juegos',['ngRoute'])
 
+// CONTROLADOR EN CARGADO DE CARGAR LOS DATOS EN LA GRILLA
 app.controller('ctrl_principal',function($http,$scope, $window , $location){
     
 		$http.get('jsonTest.json').then(function(response){
 				$scope.data=response.data;
 		});
+  
     
-    $scope.eliminacion = function ($window , $location) {
-        $window.alert("Vas a ver un ejemplo Sencillo");
-//        $location.path("/singleElimination");
-    };
     
-    $scope.alert = function ( $window ) {
-        $window.alert("hola");
-        
-    };
 });
+
+//Controlador de eventos
+app.controller('ctrl_events', function ($scope , $window , $location) {
+    
+    
+    
+})
+
+// CONFIG DEL SITIO
 
 app.config(function ($routeProvider) {
     $routeProvider.when("/" , {
